e2b3332cbd407124a4b1f7704127cc93f21b2523
Sean T. Klei <stk1151@rit.edu>
2018-02-16 11:02:09 -0500

    work from within class. Updated it so that we use the classes API to grab information and display in in the page. Also started updating the .css to be good

--- js/site.js
+++ js/site.js
@@ -1,24 +1,52 @@
 window.addEventListener('load', function() {
-	var zipCodeList = get_zipcode_list();
+	var apiUrl = "http://www.se.rit.edu/~swen-344/activities/rest/RESTAPI-Weather.php"
+	get_api_zipcode_list(function(zipCodeList){
+		var linkContainer = document.getElementById('zipcode-container');
+		zipCodeList.forEach(function(zipcode) {
+			var link = document.createElement('a');
+			link.id = 'zipcode_' + zipcode;
+			link.innerHTML = zipcode;
+			link.addEventListener('click', zipCodeClickHandler);
 
-	var linkContainer = document.getElementById('zipcode-container');
-	zipCodeList.forEach(function(zipcode) {
-		var link = document.createElement('a');
-		link.id = 'zipcode_' + zipcode;
-		link.innerHTML = zipcode;
-		link.addEventListener('click', zipCodeClickHandler);
-
-		linkContainer.appendChild(link);
+			linkContainer.appendChild(link);
+		});
 	});
 
 
+	function get_api_zipcode_list(callback) {
+		var url = apiUrl + '?action=get_weather_list';
+		$.get(url, function(data) {
+			data = JSON.parse(data);
+			var zipcodes = data.reduce(function(array, obj){
+				array.push(obj.zip);
+				return array;
+			}, []);
+			
+			callback(zipcodes);
+		});
+	}
+	
+	function get_api_data_by_zipcode(zipcode, callback) {
+		var url = apiUrl + '?action=get_weather&zip=' + zipcode;
+		$.get(url, function(data) {
+			data = JSON.parse(data);
+			callback(data);
+		});
+	}
+	
+	/**************************
+	 * Interaction Handlers
+	 **************************/
+	
 	function zipCodeClickHandler(event) {
 		var target = event.target;
 		var zipcode = target.id.replace('zipcode_', '');
-		var data = get_forecast_by_zipcode(zipcode);
+		get_api_data_by_zipcode(zipcode, function(data) {
+			document.getElementById("name").innerHTML = data.name;
+			document.getElementById("forecast").innerHTML = data.forecast;
+			document.getElementById("image").src = data.image;
+		});
 		
-		document.getElementById("Temperature").innerHTML = data.temperature;
-		document.getElementById("Humidity").innerHTML = data.humidity;
-		document.getElementById("Weather").innerHTML = data.weather;
+		
 	};
 });
